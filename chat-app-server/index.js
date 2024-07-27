01807ac136ebdf0307a773b1976943616982bdc9
Shaharyar <shaharyar.umrani@royalcyber.com>
2024-07-27 23:53:42 +0500

    main: configured opeai

--- chat-app-server/index.js
+++ chat-app-server/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openaiRoutes from "./routes/openai.js";
 
 //Configurations
 dotenv.config();
@@ -15,6 +17,15 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+//Open AI Configuration
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
+
+//Routes
+app.use("/openai", openaiRoutes);
 
 //Server Setup
 const PORT = process.env.PORT || 9000;
---
+++ chat-app-server/routes/openai.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+import { openai } from "..";
+
+dotenv.config();
+const router = express.Router();
+
+router.post("/text", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+    console.log("text", text);
+    res.status(200).json({ text });
+  } catch (err) {
+    console.log("error : ", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+export default router;
